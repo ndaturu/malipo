b57cf4491f75e8a32a8c16d98550b1e6b1fc4fb7
paulmalgado <86109340+paulmalgado@users.noreply.github.com>
2021-06-18 15:21:07 +0300

    Update dd.php

--- malipo/dd.php
+++ malipo/dd.php
@@ -1,5 +1,3 @@
 <?php
     $con = new mysqli('localhost','root','','miamala_db');
-//for user informationa making available for all pages
-	$base_path = "http://localhost:8080/dev.test/miamala_db";
 ?>
